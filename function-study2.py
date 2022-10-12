4d7824ba64743cfcb023f7852a6b700227f5c149
middle <zhongtingliang@yeah.net>
2022-10-12 10:48:19 +0800

    study

--- function-study2.py
+++ function-study2.py
@@ -1,6 +1,8 @@
 #python中的五种数据容器：
 #列表list， 元组touple， 字符串str， 集合set， 字典dict
 
+#列表
+
 """name_list = ["df","dg","as"]
 name_list[0] = 1 #修改元素
 name_list.insert(2,"dfgh") #插入元素
@@ -50,7 +52,7 @@ anyway_list5.clear() #清空列表内容
 print(anyway_list5)
 
 anyway_list6 = ["df","dg","as","df"]
-print(anyway_list6.count("df")) #统计某元素在列表内的数量"""
+print(anyway_list6.count("df")) #统计某元素在列表内的数量
 
 anyway_list8 = ["df","dg","as","df"]
 print(len(anyway_list8))
@@ -64,4 +66,34 @@ while index < len(anyway_list8):
 
 for i in anyway_list8:
     print(i)
+
+#元组touple
+
+t1 = (1,"hey",True)
+t2 = ("hey",) #元组内只有一个数据时，数据后面也要加逗号
+print(t1,t2)
+
+t3 = ((1,"hey",True),(1,2,3))
+print(t3)
+
+t4 = (1,2,"hey",2,3,4,"hey")
+print(t4[2])
+print(t4.index(2))
+print(t4.count("hey"))
+print(t4.count(2))
+print(len(t4))"""
+
+#字符串
+str1 = "ituj and ti itfgh"
+print(str1.count("it")) #count用于统计字符串中某字符出现的次数，其中it是作为一个整体统计
+
+str2 = "12fgdfh and fdyfdy21"
+print(str2.strip("12")) #字符串的规整操作，字符串.strip（字符串）是去前后指定字符串，传入的是“12”，其实就是1和2都会被移除，是按照单个字符
+
+str3 = " ituj and ti itfgh "
+print(str3.strip()) #字符串的规整操作，字符串.strip（）是去前后空格
+print(len(str3)) #空格，数字，符号，中文，字母算在字符串的长度里面
+
+#集合set
+
 
